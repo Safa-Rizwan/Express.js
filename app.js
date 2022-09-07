7904787b567862be6948761bf34a72ca25a24c4c
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-09-07 23:56:15 +0500

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { createSecretKey } = require("crypto");
 const express = require("express");
+const { get } = require("http");
 const app = express();
 const port = 80;
 
@@ -20,8 +21,34 @@ app.post("/about",(req,res)=>{
 })
 */
 // status code
+/*
 app.get("/eror",(req,res)=>{
     res.status(404).send("file not found Error!")
+})
+*/
+
+// res.write + html
+/*
+app.get("/",(req,res)=>{
+    res.write("<h1> welcome to the home page</h1>");
+    res.write("this is not a html tag simple text");
+    res.send();
+})
+*/
+// passing object/array into send that will convert into JSON
+/*app.get("/",(req,res)=>{
+    res.send({
+        id:1,
+        name:"Safa"
+    })
+})*/
+
+// res.send() == res.JSON
+app.get('/',(req,res)=>{
+    res.json({
+        id:1,
+        name:"Safa"
+    })
 })
 app.listen(port,()=>{
     console.log(`your request is listening at port number ${port}` );
